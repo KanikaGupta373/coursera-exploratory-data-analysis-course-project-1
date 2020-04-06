8483b91dbf1b7ab977fbe300db7adb97eb3249fd
Reetu Karmakar <reetu.p19218@iimtrichy.ac.in>
2020-04-06 12:44:52 +0530

    first committ

--- plot4.R
+++ plot4.R
@@ -1,4 +1,7 @@
-dataFile <- "./data/household_power_consumption.txt"
+#this is to plot the four graphs of Global active power, voltage,energy 
+##submetering and Global reactive power
+
+dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
@@ -22,7 +25,7 @@ plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 
